a4f46e9a1feee4a85db01aebb8db5e53889ffeb1
3096 <the3096@gmail.com>
2017-05-16 02:59:55 -0700

    Someone should have told me there's a sdmc_getmtime #facepalm

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # NTR-Screenshot-Mover
 A simple 3DS homebrew app to move NTR Screenshots out of SD root directory.
 
+## Why?
+
+NTR uses a naming convention of "top/bot_<INDEX>.bmp", which means that everytime NTR boots, it needs to scan the entire directory of screenshots in order to figure out the index. This process, plus 3DS's slow file system, can take up to **minutes** when there are a lot of screenshot. Plus in general, having that many files in the SD root directory is not a good idea.
+
 ## To do list:
 
-- Actually move something
-- Detect time of when the file is created for naming
+- Actually move something (WIP)
+- Detect time of when the file is created for naming (WIP)
 - Optionally, launch BootNTRSelector when done
 
 ## Possible features, no guarantee:
--- source/main.c
+++ source/main.c
@@ -1,25 +1,22 @@
 #include <string.h>
 #include <dirent.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 #include <3ds.h>
 
-void getFileCreationTime(char *filePath)
-{
-    struct stat attrib;
-    stat(filePath, &attrib);
-    char date[10];
-    strftime(date, 10, "%d-%m-%y", gmtime(&(attrib.st_mtime)));
-    printf("The file %s was last modified at %s\n", filePath, date);
-    date[0] = 0;
+// get file modification time in a formated string
+char * getFileTime(char filePath[]) {
+	u64 mtime;
+	sdmc_getmtime(filePath, &mtime);
+	// function not finished, needs format string
+	return NULL;
 }
 
+// function name not final
 void mvFiles() {
-	const char TOP_SS_NAME_PREFIX[5] = "top_";
-	const char BOT_SS_NAME_PREFIX[5] = "bot_";
-	const char SS_FORMAT[5] = ".bmp";
+	const char *TOP_SS_NAME_PREFIX = "top_";
+	const char *BOT_SS_NAME_PREFIX = "bot_";
+	const char *SS_FORMAT = ".bmp";
 
 	DIR           *d;
 	struct dirent *dir;
@@ -29,15 +26,27 @@ void mvFiles() {
 	{
 		while ((dir = readdir(d)) != NULL)
 		{
-			char cFilePrefix[5];
-			strncpy(cFilePrefix, dir->d_name, 4);
-			cFilePrefix[4] = '\0';
-			char cFileFormat[5];
-			memcpy(cFileFormat, &dir->d_name[8], 4);
-			cFileFormat[4] = '\0';
+			char *cFileFormat = strrchr(dir->d_name, '.');
+			if(cFileFormat && strcmp(cFileFormat, SS_FORMAT) == 0) {
 
-			if (strcmp(cFileFormat, SS_FORMAT) == 0) {
+				char cFilePrefix[5];
+				strncpy(cFilePrefix, dir->d_name, 4);
+				cFilePrefix[4] = '\0';
 				
+				printf("%s\n", cFilePrefix);
+
+				char cFilePath[strlen(dir->d_name) + 1];
+				strcpy(cFilePath, "/");
+				strcat(cFilePath, dir->d_name);
+
+				// Code below is unfinied
+				getFileTime(cFilePath);
+
+				if (strcmp(cFilePrefix, TOP_SS_NAME_PREFIX) == 0) {
+
+				} else if (strcmp(cFilePrefix, BOT_SS_NAME_PREFIX) == 0) {
+
+				}
 			}
 		}
 
@@ -53,14 +62,11 @@ int main(int argc, char **argv) {
 	printf("Press A to fail\n");
 	printf("Press START to rage quit\n");
 
-
 	// Main loop
 	while (aptMainLoop()) {
 
 		gspWaitForVBlank();
 		hidScanInput();
-
-		// Your code goes here
 
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
